1bcd7d8324f61c6514ac1e9b7fedd29b61a82d89
William <william.seipp@gmail.com>
2025-05-06 11:20:33 -0700

    change tabs to 2 spaces

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 --[[
-
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
@@ -110,6 +109,11 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- tabs are 2 spaces
+vim.opt.tabstop = 2 -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 2 -- Number of spaces for each indentation level
+vim.opt.softtabstop = 2 -- Number of spaces a <Tab> uses while editing
+vim.opt.expandtab = true -- Insert spaces instead of tab characters
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
